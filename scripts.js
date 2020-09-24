fa0570865fa7911b13ef3d431d8bc7ecc55174da
AvijeetDey96 <avijeetdey96@gmail.com>
2020-09-24 12:57:11 +0530

    Made Lyrics Application

--- scripts.js
+++ scripts.js
@@ -5,13 +5,16 @@ const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 let  firstPage = true;
 let  SecondPage = false;
-const  lyricsInformation = []; 
+let   lyricsInformation = []; 
+let  lyrics = '';
 const list =  document.querySelector('#list')
+let dispalyLyrics =  document.querySelector('#dispalyLyrics')
+
 const row = document.querySelector('.card');
- 
+screenSelector();
   function  screenSelector(){
     
-   if(SecondPage == true){
+   if(SecondPage == true && firstPage == false){
     document.querySelector('.container').style.display = "none";
     document.querySelector('.lyrics').style.display = "block";
   }
@@ -28,38 +31,80 @@ function setQuery(evt) {
 function getResults (query) {
     fetch(`${api.base}suggest/${query}`, {mode: 'cors'})
       .then(lyricsData => {
+        lyricsInformation = [];
+        document.addEventListener("DOMContentLoaded", getResults);
+
         return lyricsData.json();
         
       }).then(displayResults);
   }
   
 function displayResults (data) {
- this.lyricsInformation = data.data;
- this.lyricsInformation.forEach((info )=> addLyricsInfoToList(info));
+  if(data.data.length != 0){
+  lyricsInformation = data.data;
+  document.getElementsByClassName("list").innerHTML = "";
+  list.innerHTML = '' 
+  lyricsInformation.forEach((info )=> addLyricsInfoToList(info));
+ }
+ else{
+  }
+
  }
 
-  function getLyrics(event){
-     SecondPage = true;
-   this.screenSelector();  
+  function getLyrics(name,title){
+       fetch(`${api.base}/v1/${name}/${title}`, {mode: 'cors'})  
+     .then(lyrics => { 
+      return lyrics.json()
+     }).then(dispatchLyrics) 
+    
+}
+function dispatchLyrics(data){
 
+
+if(data.lyrics == ""){
+  lyrics ="No Lyrics Found!"
+}
+else{
+  lyrics = data.lyrics;
+}
+SecondPage = true;
+firstPage =false;
+screenSelector();
+showLyrics(lyrics);
 }
 function addLyricsInfoToList(info){
-  console.log(info);
+  
 list.innerHTML +=`
-<div class="card" style="cursor:pointer" onclick="getLyrics(' ${info.artist.name} ')"
+<div class="card col-lg-4 col-md-4 col-sm-6 col-xs-12" style="cursor:pointer" onclick="getLyrics(' ${info.artist.name}' , ' ${info.album.title}')"
 
-  <div class="container">
-  <img src="${info.album.cover_big}" alt="Avatar" style="width:100%">
+  <div class="container card-design">
+  <img src="${info.album.cover_medium}" alt="Avatar" >
 
-    <h4><b>${info.artist.name}</b></h4> 
+    <p style="font-size:15px;padding:0;    text-align: center;    font-weight: bold;">${info.artist.name}</p> 
     
-        <p>${info.title}</p> 
+        <p style="font-size:15px;padding:0;    text-align: center;  font-weight: bold;  margin-bottom: 5px;">${info.title}</p> 
   </div>
 </div>
-<br/>
+ 
 `;
 
  }
+ function showLyrics(lyrics) {
+  dispalyLyrics.innerHTML =`
+  <divclass="container info-text" >
+  ${lyrics}
+  </div>
+  `;
+ }
+ function BackToHome(){
+  SecondPage = false;
+  firstPage =true;
+  screenSelector();
+  document.addEventListener('DOMContentLoaded',displayResults)
+
+ }
+ document.addEventListener('DOMContentLoaded',addLyricsInfoToList)
 
 document.addEventListener('DOMContentLoaded',displayResults)
 
+ 
